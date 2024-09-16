e9d1bcea975395ed85b169f9751e06f1779fa14d
Return-Log <chnlmr@outlook.com>
2024-09-16 21:01:02 +0800

    ui文件运行

---
+++ 1 程序入门实例/ui文件的运行.py
@@ -0,0 +1,9 @@
+from PyQt6.QtWidgets import QApplication
+from PyQt6 import uic
+import sys
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ui = uic.loadUi("./ui文件的运行.ui")
+    ui.show()
+    sys.exit(app.exec())
